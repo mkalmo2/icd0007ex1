af3ff2f341a95a94ca64f54ce071779a5ae4b057
Märt Kalmo <mart.kalmo@gmail.com>
2023-02-02 14:12:16 +0200

    fixed comment

--- ex4.php
+++ ex4.php
@@ -8,6 +8,6 @@ function stringToIntegerList(string $input): array {
     return [];
 }
 
-//// check that the restored list is the same as the input list.
-//print var_dump($list === [1, 4, 2, 0]); // should print "bool(true)"
+// check that the restored list is the same as the input list.
+// var_dump($list === [1, 4, 2, 0]); // should print "bool(true)"
 
